21a069c8526c75a871173793ec5928f002fed5ac
Akarsh Kumar <akarshkumar0101@gmail.com>
2023-07-30 05:22:58 -0400

    distilling transformer into rnn

--- transformer-distilation/distill.py
+++ transformer-distilation/distill.py
@@ -6,31 +6,68 @@ from tqdm.auto import tqdm
 import train
 import models
 
-device = 'mps'
+import argparse
+from distutils.util import strtobool
 
-teacher_gpt = models.GPT(33, 32, 768, 4, 12)
-teacher_gpt.load_state_dict(torch.load("models/gpt.pt"))
-teacher_gpt = teacher_gpt.eval()
+parser = argparse.ArgumentParser()
+parser.add_argument("--track", type=lambda x: bool(strtobool(x)), default=False)
+parser.add_argument("--entity", type=str, default=None)
+parser.add_argument("--project", type=str, default="transformer-distillation-toy")
+parser.add_argument("--name", type=str, default=None)
+# parser.add_argument("--log-video", type=lambda x: bool(strtobool(x)), default=False)
+# parser.add_argument("--log-hist", type=lambda x: bool(strtobool(x)), default=False)
 
-teacher_comp = models.CompressionGPT(33, 32, 768, 4, 12, mode="random-causal")
-state_dict = torch.load("models/random-causal.pt")
-state_dict["mask"] = teacher_comp.mask
-teacher_comp.load_state_dict(state_dict)
-teacher_comp = teacher_comp.eval()
+parser.add_argument("--device", type=str, default="cpu")
+parser.add_argument("--seed", type=int, default=0)
 
-teacher = teacher_comp
-teacher = teacher.to(device)
+parser.add_argument("--n-iters", type=lambda x: int(float(x)), default=int(1000))
 
-def main():
+parser.add_argument("--vocab-size", type=int, default=33)
+parser.add_argument("--block-size", type=int, default=32)
+parser.add_argument("--n-embd", type=int, default=64)
+parser.add_argument("--n-layer", type=int, default=4)
+parser.add_argument("--n-head", type=int, default=4)
+
+parser.add_argument("--model", type=str, default="gpt")
+parser.add_argument("--mode", type=str, default="random")
+
+parser.add_argument("--lr", type=float, default=3e-5)
+parser.add_argument("--batch-size", type=int, default=16)
+parser.add_argument("--dropout", type=float, default=0.0)
+
+parser.add_argument("--save-model", type=str, default=None)
+
+
+def parse_args(*args, **kwargs):
+    args = parser.parse_args(*args, **kwargs)
+    if args.name:
+        args.name = args.name.format(**vars(args))
+    return args
+
+
+def main(args):
+    teacher_gpt = models.GPT(33, 32, 768, 4, 12)
+    teacher_gpt.load_state_dict(torch.load("models/gpt.pt"))
+    teacher_gpt = teacher_gpt.eval()
+
+    teacher_comp = models.CompressionGPT(33, 32, 768, 4, 12, mode="random-causal")
+    state_dict = torch.load("models/random-causal.pt")
+    state_dict["mask"] = teacher_comp.mask
+    teacher_comp.load_state_dict(state_dict)
+    teacher_comp = teacher_comp.eval()
+
+    teacher = teacher_comp
+    teacher = teacher.to(args.device)
+
     # net = models.MyOneStepRecurrentNet(33, 768, 4 + 1)
     net = models.MyOneStepRecurrentTransformer(33, 768, 6, 12)
-    net = net.to(device)
-    opt = torch.optim.Adam(net.parameters(), lr=1e-4)
+    net = net.to(args.device)
+    opt = torch.optim.Adam(net.parameters(), lr=args.lr)
 
     data = []
     for i_iter in tqdm(range(10000)):
         bs = 128 if i_iter % 200 == 0 else 16
-        tok = train.generate_batch(33, 33, bs, device='cpu').to(device)
+        tok = train.generate_batch(33, 33, bs, device=args.device)
         x, y = tok[:, :-1], tok[:, 1:]
 
         with torch.no_grad():
@@ -58,4 +95,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main(parse_args())
--- transformer-distilation/run.sh
+++ transformer-distilation/run.sh
@@ -6,5 +6,7 @@
 # python main.py --track=True --name="{model}_{mode}" --device=cpu --model=compress-gpt --mode=random-full   --n-iters=500 --n-embd=768 --n-layer=4 --n-head=12
 # python main.py --track=True --name="{model}_{mode}" --device=cpu --model=compress-gpt --mode=half-full     --n-iters=500 --n-embd=768 --n-layer=4 --n-head=12
 
-python main.py --track=False --name="{model}_{mode}" --device=cpu --model=gpt          --mode=_             --n-iters=1000 --n-embd=768 --n-layer=4 --n-head=12 --save-model="models/gpt.pt"
-python main.py --track=False --name="{model}_{mode}" --device=cpu --model=compress-gpt --mode=random-causal --n-iters=1000 --n-embd=768 --n-layer=4 --n-head=12 --save-model="models/random-causal.pt"
+python train.py --track=True --name="{model}_{mode}_0" --device=cuda:0 --model=gpt          --mode=_             --n-iters=5000 --n-embd=768 --n-layer=4 --n-head=12 --save-model="models/gpt.pt"
+python train.py --track=True --name="{model}_{mode}_0" --device=cuda:1 --model=compress-gpt --mode=random-causal --n-iters=5000 --n-embd=768 --n-layer=4 --n-head=12 --save-model="models/random-causal.pt"
+
+python train.py --track=False --name="{model}_{mode}_0" --device=cuda:0 --model=gpt          --mode=_             --n-iters=5000 --n-embd=768 --n-layer=4 --n-head=12 --save-model="models/gpt.pt"
